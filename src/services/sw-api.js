8608c4d0141df84ef35701ed83fb0bb11bce97ec
tpreston96 <tpreston96@gmail.com>
2021-01-07 17:53:37 -0600

    added details function

--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -4,4 +4,8 @@ export function getAllStarships() {
   return fetch(`${baseUrl}/api/starships`).then((res) => res.json());
 }
 
+export function getStarshipDetails(apiUrl) {
+  return fetch(`${apiUrl}`).then((res) => res.json());
+}
+
 console.log(getAllStarships());
